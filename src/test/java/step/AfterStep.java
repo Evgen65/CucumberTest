05358a1cf8c0da6fefdf44463f2876bcd45c88ed
Oleg Pendrak <38357504+penolegrus@users.noreply.github.com>
2022-06-13 15:36:19 +0500

    Update AfterStep.java

--- src/test/java/step/AfterStep.java
+++ src/test/java/step/AfterStep.java
@@ -6,11 +6,6 @@ import io.cucumber.java.After;
 
 public class AfterStep {
 
-    @After
-    public void tearDown(){
-        WebDriverRunner.getWebDriver().quit();
-    }
-
     @io.cucumber.java.AfterStep
     public void makeScreenshot(){
         Selenide.screenshot(System.currentTimeMillis() + "step");
